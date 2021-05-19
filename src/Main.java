ec515d2edfaa692cb21a7a761d8c3dbf076dd003
VToporan <80809332+VToporan@users.noreply.github.com>
2021-05-19 23:52:12 +0300

    Adds fxml for start window and popup

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource = getClass().getClassLoader().getResource("StartWindow.fxml");
+        if (resource != null) {
+            Parent start_layout = FXMLLoader.load(resource);
+            //noinspection SpellCheckingInspection
+            primaryStage.setTitle("Uwuntu App");
+            primaryStage.setScene(new Scene(start_layout, 300, 200));
+            primaryStage.show();
+        }
+    }
+}
